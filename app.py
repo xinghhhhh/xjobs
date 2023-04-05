126d2ace3147336674de08b246d5e2ea461e6eca
xinghhhhh <943580088@qq.com>
2023-04-06 00:54:37 +0800

    update

--- app.py
+++ app.py
@@ -5,15 +5,13 @@ import json
 app = Flask(__name__)
 
 session =db.get_seesion()
-all_jobs = session.execute("SELECT job_name,repeat_flag from xjobs_job")
+all_jobs = session.execute("SELECT job_name,repeat_flag,status,today_flag from xjobs_job")
 today_jobs = session.execute("SELECT job_name,status from xjobs_job where today_flag='Y'")
-running_jobs = session.execute("SELECT job_name,status from xjobs_job where status='RUNNING'")
 all_jobs_rely = session.execute("SELECT job_name,rely from xjobs_job").all()
 session.commit()
-
 a_jobs=all_jobs.all()
 t_jobs=today_jobs.all()
-r_jobs=running_jobs.all()
+
 
 def covert_to_list(target):
     list = []
@@ -26,111 +24,27 @@ def covert_to_list(target):
 
 
 all_j = covert_to_list(a_jobs)
-today_j = covert_to_list(today_jobs)
-running_j = covert_to_list(running_jobs)
+today_j = covert_to_list(t_jobs)
 
-#all_j =[{name:value} for name,value in zip(key_list,value_list)]
-
-graph_a={
-            "nodes": [{
-                    "id": 0,
-                    "label": "流动人员",
-                    "shape": "rect",
-                    "name": "dddddddddddddddd"
-                },
-                {
-                    "id": 1,
-                    "label": "安全筛查",
-                    "shape": "rect",
-                    "name": "tttt"
-                },
-                {
-                    "id": 2,
-                    "label": "热像仪人体测温筛查",
-                    "shape": "diamond",
-                    "name": "ertw"
-                },
-                {
-                    "id": 3,
-                    "label": "人工复测",
-                    "shape": "diamond",
-                    "name":"hcfg"
-                },
-                {
-                    "id": 4,
-                    "label": "快速通过",
-                    "shape": "rect",
-                    "name": "rety"
-                },
-                {
-                    "id": 5,
-                    "label": "紧急处理",
-                    "shape": "rect",
-                    "name": "asda1"
-                }
-            ],
-            "edges": [{
-                    "source": 0,
-                    "target": 1,
-                    "label": ""
-                },
-                {
-                    "source": 1,
-                    "target": 2,
-                    "label": ""
-                },
-                {
-                    "source": 2,
-                    "target": 4,
-                    "label": "正常"
-                },
-                {
-                    "source": 3,
-                    "target": 5,
-                    "label": "不正常"
-                },
-                {
-                    "source": 3,
-                    "target": 4,
-                    "label": "什么鬼"
-                },
-                {
-                    "source": 2,
-                    "target": 3,
-                    "label": "不正常"
-                }
-            ]
-        }
 
 test_to_json = {"nodes":[],"edges":[]}
-import itertools
 for job in all_j:
-    test_to_json["nodes"].append({"id":job[0], "label": job[0], "shape": "rect"})
+    test_to_json["nodes"].append({"id":job[0], "label": job[0], "shape": "rect", "status": job[2], "today_flag": job[3]})
 
 
-# for rely in all_jobs_rely:
-#     if rely[1] != '':
-#         print(rely[1])
-#         tmp_list = rely[1].split(',')
-#         for rely_c in tmp_list:
-#             test_to_json["edges"].append({"source": rely[0], "target": rely_c})
-#     else:
-#         continue
 
 for rely in all_jobs_rely:
     if rely[1] != '':
-        print(rely[1])
         tmp_list = rely[1].split(',')
         for rely_c in tmp_list:
             test_to_json["edges"].append({"source": rely_c, "target": rely[0]})
     else:
         continue
-
 print(test_to_json)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template("index.html", all_jobs=all_j, today_jobs=today_j, running_jobs=running_j)
+    return render_template("index.html", all_jobs=all_j, today_jobs=today_j, graph_a=test_to_json)
 
 @app.route('/tet')
 def tet():
--- static/left.js
+++ static/left.js
@@ -2,39 +2,181 @@
 function all_job(){
     var a_job=document.getElementById('all_job');
     var t_job=document.getElementById('today_job');
-    var r_job=document.getElementById('running_job');
     document.getElementById('a_page').style.backgroundColor='lightblue';
     document.getElementById('t_page').style.backgroundColor='transparent';
-    document.getElementById('r_page').style.backgroundColor='transparent';
-    //a_job.style.backgroundColor='lightblue';
     a_job.style.display='block';
     t_job.style.display='none';
-    r_job.style.display='none';
 }
 
 function today_job(){
     var a_job=document.getElementById('all_job');
     var t_job=document.getElementById('today_job');
-    var r_job=document.getElementById('running_job');
     document.getElementById('t_page').style.backgroundColor='lightblue';
     document.getElementById('a_page').style.backgroundColor='transparent';
-    document.getElementById('r_page').style.backgroundColor='transparent';
-    //t_job.style.backgroundColor='lightblue';
     a_job.style.display='none';
     t_job.style.display='block';
-    r_job.style.display='none';
 }
 
-function running_job(){
-    var a_job=document.getElementById('all_job');
-    var t_job=document.getElementById('today_job');
-    var r_job=document.getElementById('running_job');
-    document.getElementById('r_page').style.backgroundColor='lightblue';
-    document.getElementById('a_page').style.backgroundColor='transparent';
-    document.getElementById('t_page').style.backgroundColor='transparent';
-    //r_job.style.backgroundColor='lightblue';
-    a_job.style.display='none';
-    t_job.style.display='none';
-    r_job.style.display='block';
+//前端获取后台数据并呈现在<div class="ui-tab">中
+function information_display_all(page) {
+    const informationDisplay = document.querySelector(".ui-tab-all");
+    var begin =page*10-10;
+    var end = 0;
+    if (page*10 < all_count){
+        var end = page*10-1;
+    }else {
+        var end = all_count-1;
+    };
+    var innerHT = '<table id="container" style="table-layout: fixed;background-color: #bdc3c7" ' +
+        '<thead id="a_thead" style="font-size: 18px"> <tr> <td >任务名('+all_count+')</td><td style="width: 80px;text-align: center">类型</td> </tr> </thead> ' +
+        '<tbody id="a_tbody" style="background-color: #ecf0f1;font-size: 14px">';
+    for (var i=begin;i<=end;i++){
+        innerHT +='<tr ><td >';
+        innerHT += all_jobs[i][0];
+        innerHT +='</td><td style="text-align: center">';
+        innerHT += all_jobs[i][1];
+        innerHT +='</td></tr>';
+    };
+    innerHT += '</tbody> </table>';
+    informationDisplay.innerHTML = innerHT;
 }
 
+function information_display_today(page) {
+    const informationDisplay = document.querySelector(".ui-tab-today");
+    var begin =page*10-10;
+    var end = 0;
+    if (page*10 < today_count){
+        var end = page*10-1;
+    }else {
+        var end = today_count-1;
+    };
+    var innerHT = '<table id="container" style="table-layout: fixed;background-color: #bdc3c7" ' +
+        '<thead id="t_thead" style="font-size: 18px"> <tr> <td >任务名('+today_count+')</td><td style="width: 80px;text-align: center">状态</td> </tr> </thead> ' +
+        '<tbody id="t_tbody" style="background-color: #ecf0f1;font-size: 14px">';
+    for (var i=begin;i<=end;i++){
+        innerHT +='<tr ><td >';
+        innerHT += today_jobs[i][0];
+        innerHT +='</td><td style="text-align: center">';
+        innerHT += today_jobs[i][1];
+        innerHT +='</td></tr>';
+    };
+    innerHT += '</tbody> </table>';
+    informationDisplay.innerHTML = innerHT;
+}
+
+
+
+
+
+
+function highlight_all(){
+    var trs = document.querySelector('#a_tbody').querySelectorAll("tr");
+// 2. 利用循环绑定注册事件
+    for (var i = 0; i < trs.length; i++) {
+        // 3. 鼠标经过事件 onmouseover 当鼠标经过的时候
+        // 给表格添加一个背景属性
+        trs[i].onmouseover = function() {
+                this.className = 'bg';
+            }
+            // 4. 鼠标离开事件 onmouseout
+        trs[i].onmouseout = function() {
+            this.className = '';
+        }
+        trs[i].onclick = function (){
+            alert("ffffffffffffffff")
+        }
+    }
+}
+
+function highlight_today(){
+    var trs = document.querySelector('#t_tbody').querySelectorAll("tr");
+// 2. 利用循环绑定注册事件
+    for (var i = 0; i < trs.length; i++) {
+        // 3. 鼠标经过事件 onmouseover 当鼠标经过的时候
+        // 给表格添加一个背景属性
+        trs[i].onmouseover = function() {
+                this.className = 'bg';
+            }
+            // 4. 鼠标离开事件 onmouseout
+        trs[i].onmouseout = function() {
+            this.className = '';
+        }
+        trs[i].onclick = function (){
+            alert("yyyyyyyyyyyyyyyyyyyy")
+        }
+    }
+}
+
+
+
+
+//上一页、下一页，首页和尾页的单击触发事件
+function page_click_all(item) {
+    console.log(item);
+    let pageNumber=0;
+    if (item.className == "firstPage"){
+        console.log(item.className);
+        //pageNumber = document.querySelector(".currentPage").value;
+        document.querySelector(".currentPage").value=1;
+        information_display_all(1)
+    }
+    else if (item.className == "beforePage") {
+        console.log(item.className);
+        pageNumber = document.querySelector(".currentPage").value;
+        if (pageNumber > 1) {
+            document.querySelector(".currentPage").value=Number(pageNumber)-1;
+            information_display_all(Number(pageNumber)-1)
+        }
+    }
+    else if (item.className == "nextPage") {
+        console.log(item.className);
+        pageNumber = document.querySelector(".currentPage").value;
+        console.log(pageNumber);
+        console.log(typeof(pageNumber));
+        if (pageNumber < totalPage) {
+            let n=document.querySelector(".currentPage").value=Number(pageNumber)+1;
+            information_display_all(Number(pageNumber)+1)
+        }
+    }
+    else {
+        console.log(item.className);
+        document.querySelector(".currentPage").value=totalPage;
+        information_display_all(totalPage)
+    };
+    highlight_all()
+}
+
+function page_click_today(item) {
+    console.log(item);
+    let pageNumber=0;
+    if (item.className == "firstPage"){
+        console.log(item.className);
+        //pageNumber = document.querySelector(".currentPage").value;
+        document.querySelector(".currentPage_today").value=1;
+        information_display_today(1)
+    }
+    else if (item.className == "beforePage") {
+        console.log(item.className);
+        pageNumber = document.querySelector(".currentPage_today").value;
+        if (pageNumber > 1) {
+            document.querySelector(".currentPage_today").value=Number(pageNumber)-1;
+            information_display_today(Number(pageNumber)-1)
+        }
+    }
+    else if (item.className == "nextPage") {
+        console.log(item.className);
+        pageNumber = document.querySelector(".currentPage_today").value;
+        console.log(pageNumber);
+        console.log(typeof(pageNumber));
+        if (pageNumber < today_Page) {
+            let n=document.querySelector(".currentPage_today").value=Number(pageNumber)+1;
+            information_display_today(Number(pageNumber)+1)
+        }
+    }
+    else {
+        console.log(item.className);
+        document.querySelector(".currentPage_today").value=today_Page;
+        information_display_today(today_Page)
+    };
+    highlight_today()
+}
